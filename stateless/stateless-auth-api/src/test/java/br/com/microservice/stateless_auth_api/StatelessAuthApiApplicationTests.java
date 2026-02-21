bf4019001ddbda2103133d756009fd729285a6fb
Danilo Cordeiro <dev.danilocordeiro@gmail.com>
2026-02-21 08:14:26 -0300

    chore: add base projects

---
+++ stateless/stateless-auth-api/src/test/java/br/com/microservice/stateless_auth_api/StatelessAuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.microservice.stateless_auth_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatelessAuthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
